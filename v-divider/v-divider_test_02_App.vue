25a613dcdee6c139e8f8439a4336a829053dcd0d
Marcell Szabó <43202920+Marcelion1999@users.noreply.github.com>
2018-11-12 08:28:31 +0100

    Add files via upload

---
+++ v-divider/v-divider_test_02_App.vue
@@ -0,0 +1,37 @@
+new Vue({
+  el: '#app',
+  data: () => ({
+    items: [
+      {
+        action: 'move_to_inbox',
+        title: 'Inbox'
+      },
+      {
+        action: 'send',
+        title: 'Sent'
+      },
+      {
+        action: 'delete',
+        title: 'Trash'
+      },
+      {
+        action: 'report',
+        title: 'Spam'
+      },
+      { divider: true },
+      { header: 'Labels' },
+      {
+        action: 'label',
+        title: 'Family'
+      },
+      {
+        action: 'label',
+        title: 'Friends'
+      },
+      {
+        action: 'label',
+        title: 'Work'
+      }
+    ]
+  })
+})
